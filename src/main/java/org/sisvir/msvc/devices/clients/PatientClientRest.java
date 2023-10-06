331f1e4289224cee2461d4a6d327696dd303c0fd
Carlos Jiménez Domínguez <89859160+carjizo@users.noreply.github.com>
2023-10-05 22:42:41 -0500

    Config application-prod.properties

--- src/main/java/org/sisvir/msvc/devices/clients/PatientClientRest.java
+++ src/main/java/org/sisvir/msvc/devices/clients/PatientClientRest.java
@@ -1,11 +1,13 @@
 package org.sisvir.msvc.devices.clients;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-users", url = "localhost:8003", path = "/patients")
+
+@FeignClient(name = "msvc-users", url = "msvc-users-production.up.railway.app", path = "/patients")
 public interface PatientClientRest {
 
     @DeleteMapping("/delete-device/{id}")
